211b53425e689888ac5a2cd45edff3f9e9a86c01
LindaTancini <lindatancini95@gmail.com>
2025-05-23 11:31:05 +0200

    feat: scaffolding express

---
+++ app.js
@@ -0,0 +1,14 @@
+//Express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+//HomePage
+app.get("/", (req, res) => {
+  res.send("Homepage della Webapp!");
+});
+
+// Avvio server
+app.listen(port, () => {
+  console.log(`Sono un server attivo sulla porta:${port}`);
+});
