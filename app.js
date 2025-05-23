25ca3c6a80755e8b1faf5d825d2086a4b8f4f7ca
Carlo Costa <carlo.costa92@gmail.com>
2025-05-23 14:50:48 +0200

    feat:create middlewares

--- app.js
+++ app.js
@@ -2,11 +2,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const errorHandler = require('./middlewares/errorHandler.js');
+const notFound = require('./middlewares/notFound.js');
 
 //HomePage
 app.get("/", (req, res) => {
   res.send("Homepage della Webapp!");
 });
+
+// middleware errori
+app.use(errorHandler);
+
+// middleware rotte non trovate
+app.use(notFound);
 
 // Avvio server
 app.listen(port, () => {
