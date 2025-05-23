9e50812937f545fc2141e8f895997d83d7729888
micheledidonna01 <micheledidonna337@gmail.com>
2025-05-23 17:30:54 +0200

    add: genre e artist to show

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -29,7 +29,11 @@ function index(req, res) {
 function show(req, res) {
     const { id } = req.params;
 
-    const sql = 'SELECT * FROM albums WHERE albums.id = ?';
+    const sql = `SELECT albums.*, genres.name AS 'genre', artists.name AS 'artist'
+                    FROM albums 
+                    INNER JOIN genres ON genres.id = albums.genre_id
+                    INNER JOIN artists ON artists.id = albums.id_artist
+                    WHERE albums.id = ?`;
 
     connection.query(sql, [id], (err, result) => {
         if (err) {
@@ -37,7 +41,12 @@ function show(req, res) {
             return res.status(500).json({ error: 'Database query failed', details: err.message });
         }
         if (result.length === 0) return res.status(404).json({ errorMessage: 'Error Server' })
-        res.json(result[0]);
+        let album = result[0];
+        res.json({
+            ...album,
+            genre: album.genre,
+            artist: album.artist
+        });
 
     })
 }
