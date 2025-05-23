376817b017bdbaff7a99e4cdec40a92f404d8f62
Carlo Costa <carlo.costa92@gmail.com>
2025-05-23 16:12:54 +0200

    fix: db call

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Express
+require('dotenv').config();
 const port = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,20 +1,22 @@
-const connection = require("../data/db")
+const connection = require("../data/db");
 
 function index(req, res) {
-
     const sql = 'SELECT * FROM albums';
 
     connection.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: 'Database query failed' });
-        res.json(results.map((albums) => {
+        if (err) {
+            console.error("Errore nella query al database:", err);
+            return res.status(500).json({ error: 'Database query failed', details: err.message });
+        }
+        res.json(results.map((album) => {
             return {
-                id: albums.id,
-                title: albums.name,
-                cover: albums.cover
-            }
+                id: album.id,
+                title: album.name,
+                cover: album.cover
+            };
         }));
-    })
-};
+    });
+}
 
 module.exports = {
     index
