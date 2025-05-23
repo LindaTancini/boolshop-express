000efded435d5fbb4e2d43c03938f4de4bee6eb7
micheledidonna01 <micheledidonna337@gmail.com>
2025-05-23 17:07:03 +0200

    feat: modify sql index

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -2,28 +2,30 @@ const connection = require("../data/db");
 
 //index
 function index(req, res) {
-    const sql = 'SELECT * FROM albums';
+    // const sql = `SELECT * FROM albums`
+    const sql = `SELECT albums.*, genres.name AS 'genre'
+                    FROM albums 
+                    INNER JOIN genres ON genres.id = albums.genre_id
+                    ORDER BY albums.id ASC`;
 
-    console.log(process.env)
 
     connection.query(sql, (err, results) => {
         if (err) {
             console.error("Errore nella query al database:", err);
             return res.status(500).json({ error: 'Database query failed', details: err.message });
         }
-        res.json(results.map((album) => {
-            return {
-                id: album.id,
-                title: album.name,
-                cover: album.cover
-            };
-        }));
+        res.json(results.map((album) => ({
+            id: album.id,
+            title: album.name,
+            cover: album.cover,
+            genre: album.genre
+        })));
     });
 }
 
 //show
-function show(req, res){
-    const {id} = req.params;
+function show(req, res) {
+    const { id } = req.params;
 
     const sql = 'SELECT * FROM albums WHERE albums.id = ?';
 
