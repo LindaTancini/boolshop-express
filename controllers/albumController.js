07c3d101a5db0cb065f933932b8d1fd25538b819
micheledidonna01 <micheledidonna337@gmail.com>
2025-05-27 20:06:37 +0200

    add: search query params to albumController

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -2,7 +2,11 @@ const connection = require("../data/db");
 
 //index
 function index(req, res) {
-  const sql = `
+    const { search } = req.query;
+
+    const preparedParams = [];
+
+    let sql = `
     SELECT 
       album.*,
       genres.slug AS genre_slug, 
@@ -12,37 +16,44 @@ function index(req, res) {
     FROM album 
     INNER JOIN genres ON genres.id = album.genre_id
     INNER JOIN artist ON artist.id = album.id_artist
-    ORDER BY album.id ASC
   `;
-
-
-  connection.query(sql, (err, results) => {
-    if (err) {
-      console.error("Errore nella query al database:", err);
-      return res.status(500).json({ error: 'Database query failed', details: err.message });
+    
+    if (search) {
+        sql += `WHERE album.name LIKE ? OR genres.name LIKE ? OR artist.name LIKE ? `;
+        preparedParams.push(`%${search}%`, `%${search}%`, `%${search}%`);
     }
-    res.json(results.map((album) => ({
-      id: album.id,
-      slug: album.slug,
-      title: album.name,
-      cover: album.cover,
-      genre: {
-        slug: album.genre_slug,
-        name: album.genre_name
-      },
-      artist: {
-        slug: album.artist_slug,
-        name: album.artist_name
-      }
-    })));
-  });
+
+    console.log(search);
+
+    sql += `ORDER BY album.id ASC`;
+
+    connection.query(sql, preparedParams, (err, results) => {
+        if (err) {
+            console.error("Errore nella query al database:", err);
+            return res.status(500).json({ error: 'Database query failed', details: err.message });
+        }
+        res.json(results.map((album) => ({
+            id: album.id,
+            slug: album.slug,
+            title: album.name,
+            cover: album.cover,
+            genre: {
+                slug: album.genre_slug,
+                name: album.genre_name
+            },
+            artist: {
+                slug: album.artist_slug,
+                name: album.artist_name
+            }
+        })));
+    });
 }
 
 //show
 function show(req, res) {
-  const { slug } = req.params;
+    const { slug } = req.params;
 
-  const sql = `
+    const sql = `
     SELECT 
       album.*, 
       genres.slug AS genre_slug, 
@@ -55,30 +66,30 @@ function show(req, res) {
     WHERE album.slug = ?
   `;
 
-  connection.query(sql, [slug], (err, result) => {
-    if (err) {
-      console.error("Errore nella query al database:", err);
-      return res.status(500).json({ error: 'Database query failed', details: err.message });
-    }
-    if (result.length === 0) return res.status(404).json({ errorMessage: 'Error Server' })
-    let album = result[0];
-    res.json({
-      id: album.id,
-      slug,
-      title: album.name,
-      cover: album.cover,
-      genre: {
-        slug: album.genre_slug,
-        name: album.genre_name
-      },
-      artist: {
-        slug: album.artist_slug,
-        name: album.artist_name
-      },
-    });
-  })
+    connection.query(sql, [slug], (err, result) => {
+        if (err) {
+            console.error("Errore nella query al database:", err);
+            return res.status(500).json({ error: 'Database query failed', details: err.message });
+        }
+        if (result.length === 0) return res.status(404).json({ errorMessage: 'Error Server' })
+        let album = result[0];
+        res.json({
+            id: album.id,
+            slug,
+            title: album.name,
+            cover: album.cover,
+            genre: {
+                slug: album.genre_slug,
+                name: album.genre_name
+            },
+            artist: {
+                slug: album.artist_slug,
+                name: album.artist_name
+            },
+        });
+    })
 }
 
 module.exports = {
-  index, show
+    index, show
 };
