6fd68d26e986a72aca66ce0a7e8dcebd92d3e6c7
vmqxdv <vquintilio@gmail.com>
2025-05-27 22:36:17 +0200

    add: implement advanced filtering for albums in albumController and update routes

--- routers/routes.js
+++ routers/routes.js
@@ -6,5 +6,7 @@ const albumController = require("../controllers/albumController");
 router.get("/", albumController.index);
 // Show -> cerco un singolo album
 router.get("/:slug", albumController.show);
+// Filtro avanzato per album
+router.get("/filter/advanced", albumController.filter);
 
 module.exports = router;
